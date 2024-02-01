70ac2dfa16b3c086ee7ccd3a4ca2623f8741d381
영현 <yh97yhyh@naver.com>
2024-02-01 22:26:32 +0900

    custom search bar

---
+++ AirbnbClone/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,48 @@
+//
+//  SearchAndFilterBar.swift
+//  AirbnbClone
+//
+//  Created by 영현 on 2024/02/01.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+            
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Where to?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Text("Anywhere - Any Week - Add guests")
+                    .font(.caption2)
+                    .foregroundStyle(.gray)
+            }
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    .foregroundStyle(.black)
+            })
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundStyle(Color(.systemGray4))
+                .shadow(color: .black.opacity(0.4), radius: 2)
+        }
+        .padding()
+    }
+}
+
+struct SearchAndFilterBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchAndFilterBar()
+    }
+}
