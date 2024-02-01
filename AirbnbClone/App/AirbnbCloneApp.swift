3288b6537a327d497209273ec5dda1cfdffa4608
영현 <yh97yhyh@naver.com>
2024-02-01 21:54:52 +0900

    first commit

---
+++ AirbnbClone/App/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by 영현 on 2024/02/01.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
